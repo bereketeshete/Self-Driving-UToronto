03ec88f4434e14f81dc47f646d9367c9c40d91a8
Bereket Eshete <40177156+bereketeshete@users.noreply.github.com>
2021-12-20 03:19:03 -0600

    Update first.py

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
 
+class Bicycle(Bicycle):
+    def step(self, v, w):
+        # ==================================
+        #  Implement kinematic model here
+        # ==================================
+        self.xc_dot = v * cos(theta + beta)
+        self.yc_dot = v * sin(theta + beta)
+        self.theta_dot = v * cos(beta)* tan(delta)/L
+        self.delta_dot = w
+        self.beta = arctan(lr*tan(delta)/L)
+        pass
